815a8a6b7c2ccb53dc9a596774a35bfc46838345
Bruce Berrios <bberr022@fiu.edu>
2020-01-09 19:05:23 -0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,16 @@
+
+import pygame as pg
+
+pg.init()
+
+screen = pg.display.set_mode([800, 600])
+
+running = True
+
+while (running) :
+    for event in pg.event.get() :
+        if (event.type == pg.QUIT) :
+            running = False
+
+
+pg.quit()
