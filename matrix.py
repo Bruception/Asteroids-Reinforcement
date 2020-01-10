de15376c98a1d94402b807094778f661724ba346
Bruce Berrios <bberr022@fiu.edu>
2020-01-09 19:44:50 -0500

    Add matrix

---
+++ matrix.py
@@ -0,0 +1,58 @@
+from cStringIO import StringIO
+
+class Matrix :
+    def __init__(self, rows, columns) :
+        self.rows = rows
+        self.columns = columns
+        self.matrix = [[0 for j in range(columns)] for i in range(rows)]
+
+    def __mul__(self, other) :
+        return self.dot(other)
+
+    def __add__(self, other) :
+        return self.add(other)
+
+    def dot(self, other) :
+        newMatrix = Matrix(self.rows, other.columns)
+        sum = 0
+
+        for i in range(self.rows) :
+            for j in range(other.columns) :
+                sum = 0
+                for k in range(self.columns) :
+                    sum = sum + (self.matrix[i][k] * other.matrix[k][j])
+
+                newMatrix.matrix[i][j] = sum
+
+        return newMatrix
+
+    #Element Wise addition
+    def add(self, other) :
+        newMatrix = Matrix(self.rows, self.columns)
+
+        for i in range(self.rows) :
+            for j in range(self.columns) :
+                newMatrix.matrix[i][j] = self.matrix[i][j] + other.matrix[i][j]
+
+        return newMatrix
+
+    #Apply a function to each element in the matrix
+    def forEach(self, func) :
+        newMatrix = Matrix(self.rows, self.columns)
+
+        for i in range(self.rows) :
+            for j in range(self.columns) :
+                newMatrix.matrix[i][j] = func(self.matrix[i][j])
+
+        return newMatrix
+
+    def __str__(self) :
+        outStr = StringIO()
+
+        for i in range(self.rows) :
+            for j in range(self.columns) :
+                outStr.write(str(self.matrix[i][j]))
+                outStr.write("\t")
+            outStr.write("\n")
+
+        return outStr.getvalue()
