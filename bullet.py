dbf341dfa5779e0f161557d0c1df1158972dfe5d
Bruce Berrios <bberr022@fiu.edu>
2020-01-10 20:29:09 -0500

    Add bullet class

---
+++ bullet.py
@@ -0,0 +1,37 @@
+import pygame as pg
+import math
+
+import globals as g
+
+class Bullet :
+    def __init__(self, x, y, angle) :
+        self.x = x
+        self.y = y
+
+        self.angle = angle
+        self.vel = 250
+
+        self.width = 3
+        self.height = 3
+
+        self.size = 3
+
+        self.delete = False
+
+        self.vx = math.cos(self.angle) * self.vel
+        self.vy = math.sin(self.angle) * self.vel
+
+    def bound(self) :
+        boundX = (self.x - self.size <= 0) or (self.x + self.size >= 800)
+        boundY = (self.y - self.size <= 0) or (self.y + self.size >= 600)
+
+        self.delete = boundX or boundY
+
+    def update(self, dt) :
+        self.x += self.vx * dt
+        self.y += self.vy * dt
+
+        self.bound()
+
+    def draw(self, screen) :
+        pg.draw.circle(screen, g.white, [int(self.x), int(self.y)], self.size)
